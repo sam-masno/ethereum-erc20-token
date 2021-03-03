76f7cc4dbfda50dfc6e7d6fce4da08c0bbce2f4e
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-03 19:22:21 +0400

    added name, decimals, balance assignments in constructor

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,9 +2,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Token {
   uint public totalSupply;
+  string public name;
+  uint public decimal;
+  string public symbol;
+  address public wallet;
+  mapping (address => uint) balance;
+  constructor(string memory _name, string memory _symbol, uint _totalSupply,  uint _decimal) public {
+    wallet = msg.sender;
+    name = _name;
+    symbol = _symbol;
+    totalSupply = _totalSupply;
+    decimal = _decimal;
+    balance[wallet] = totalSupply;
+  }
 
-  constructor() public {
-    totalSupply = 1000000;
+  function balanceOf(address _owner) public returns(uint) {
+    return balance[_owner];
   }
 
 }
