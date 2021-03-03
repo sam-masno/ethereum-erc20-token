18413a5eb3bcfc5e87af9b359eb348ea078cab43
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-03 19:24:15 +0400

    added tests for new properties

--- test/Token.js
+++ test/Token.js
@@ -1,9 +1,36 @@
 const Token = artifacts.require('../contracts/Token.sol');
 
-contract('Token tests', (accounts) => {
+contract('Token tests', async (accounts) => {
+  let instance;
+  beforeEach(async () => {
+    instance = await Token.deployed();
+  })
+
+  it('sets name on deployment', async () => {
+    // let instance = await Token.deployed();
+    let name = await instance.name.call();
+    expect(name).to.equal('Test Token');
+  }) 
+
+  it('sets symbol on deployment', async () => {
+    let symbol = await instance.symbol.call();
+    expect(symbol).to.equal('TST');
+  })
+
   it('sets total supply to 1000000 on deployment', async () => {
-    let instance = await Token.deployed();
     let supply = await instance.totalSupply.call();
-    expect(supply.toNumber()).to.equal(1000000);
+    expect(supply.toNumber()).to.equal(1500000);
   });
+
+  it('sets decimal on deployment', async () => {
+    let decimal = await instance.decimal.call();
+    expect(decimal.toNumber()).to.equal(0);
+  })
+
+  it('allocates totalSupply to owner', async () => {
+    let adminBalance = await instance.balanceOf.call(accounts[0]);
+    let totalSupply = await instance.totalSupply.call();
+    expect(adminBalance.toNumber()).to.equal(totalSupply.toNumber());
+  })
+
 });
