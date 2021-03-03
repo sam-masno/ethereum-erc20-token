92fcdd74daaa0d1d7cba2705587b2ac230420bfa
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-03 19:23:15 +0400

    added args to Token deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ const Token = artifacts.require('../contracts/Token.sol');
 
 module.exports = async (deployer) => {
   const [wallet] = await web3.eth.getAccounts();
-  await deployer.deploy(Token);
+  await deployer.deploy(Token, 'Test Token', 'TST',  1500000, 0);
 }
