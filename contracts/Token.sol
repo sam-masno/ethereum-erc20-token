ae8060644b7bcac7e0397ed5f714f3a529f022d9
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-05 14:54:50 +0400

    added transfer function

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,19 +5,30 @@ contract Token {
   string public name;
   uint public decimal;
   string public symbol;
-  address public wallet;
+  string public version = "v1.0.0";
   mapping (address => uint) balance;
   constructor(string memory _name, string memory _symbol, uint _totalSupply,  uint _decimal) public {
-    wallet = msg.sender;
     name = _name;
     symbol = _symbol;
     totalSupply = _totalSupply;
     decimal = _decimal;
-    balance[wallet] = totalSupply;
+    balance[msg.sender] = totalSupply;
   }
 
   function balanceOf(address _owner) public returns(uint) {
     return balance[_owner];
+  }
+
+  event Transfer(address indexed _from, address indexed _to, uint value);
+
+  function transfer(address _to, uint _value) public returns(bool){
+    require(balance[msg.sender] >= _value, 'Insufficient balance');
+    require(balance[msg.sender] - _value < balance[msg.sender], 'Insufficient balance');
+    require(balance[_to] + _value > balance[_to], 'Balance exceeds limit, open new account');
+    balance[msg.sender] -= _value;
+    balance[_to] += _value;
+    emit Transfer (address(msg.sender), _to, _value);
+    return true;
   }
 
 }
