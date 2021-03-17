86af6f89933279ec35d046d987a924f081352f9f
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-17 21:24:27 +0400

    added endsale function and event

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -5,7 +5,7 @@ import "./Token.sol";
 
 contract TokenSale {
   uint public price;
-  address private admin;
+  address payable private admin;
   Token public token;
   uint public sold;
 
@@ -17,6 +17,7 @@ contract TokenSale {
   }
 
   event Sale(address _buyer, uint _amount, uint _value);
+  event EndSale(address _contract, uint _totalSold);
 
   function isAdmin() public returns(bool) {
     if(address(msg.sender) == admin) {
@@ -32,15 +33,28 @@ contract TokenSale {
   }
   
   function buyTokens(uint _numOfTokens) public payable {
-    //check sufficient payment
-    //check balance
-    // track tokens sold
-    //require transfer successful
+    // use safe math to get total
     uint total = multiply(_numOfTokens, price);
+    // assert value is adequate
     require(msg.value >= total, "Insufficient payment");
     require(msg.value == total, "Must pay exact amount");
-    require(token.balanceOf(admin) >= _numOfTokens, "Not enough tokens left");
+    //ensure contract has balance to support sale
+    require(token.balanceOf(address(this)) >= _numOfTokens, "Not enough tokens left");
+
+    // track sold tokens
     sold += _numOfTokens;
+
+    //init transfer and emit event
+    token.transfer(address(msg.sender), _numOfTokens);
     emit Sale(address(msg.sender), _numOfTokens, msg.value);
+  }
+
+  function endSale() public {
+    require(address(msg.sender) == admin, "Must be admin");
+    //transfer remaining tokens to admin
+    require(token.transfer(admin, token.balanceOf(address(this))));
+    //destroy contract
+    emit EndSale(address(this), sold);
+    selfdestruct(admin);
   }
 }
