7c70e4be48b4ff4621da4c932dc0a64c3c464ef8
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-17 19:22:17 +0400

    added basic deployment for tokenSale contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 const Token = artifacts.require('../contracts/Token.sol');
+const TokenSale = artifacts.require('../contract/TokenSale.sol');
 
 module.exports = async (deployer) => {
   const [wallet] = await web3.eth.getAccounts();
-  await deployer.deploy(Token, 'Test Token', 'TST',  1500000, 0);
+  const token = await deployer.deploy(Token, 'Test Token', 'TST',  1500000, 0);
+  await deployer.deploy(TokenSale, token.address, 1000);
 }
