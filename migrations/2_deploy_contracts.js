3627300b235b8dcff64175fc5cb867f659b40e59
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-17 21:25:18 +0400

    added tokensale and token transfer

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,8 @@ const Token = artifacts.require('../contracts/Token.sol');
 const TokenSale = artifacts.require('../contract/TokenSale.sol');
 
 module.exports = async (deployer) => {
-  const [wallet] = await web3.eth.getAccounts();
-  const token = await deployer.deploy(Token, 'Test Token', 'TST',  1500000, 0);
-  await deployer.deploy(TokenSale, token.address, 1000);
+  const [admin] = await web3.eth.getAccounts();
+  const token = await deployer.deploy(Token, 'Test Token', 'TST',  1000000, 0);
+  const tokenSale = await deployer.deploy(TokenSale, token.address, 1000);
+  token.transfer(tokenSale.address, 500000, { from: admin })
 }
