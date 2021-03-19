edc7546c1364cbbea4b49ac62a2c1c344e187c39
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-19 20:54:50 +0400

    serve /contracts with contract data

--- client/server.js
+++ client/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const data = require(('./contractData.js'));
+
 app.use('/', express.static(path.join(__dirname, 'public')));
-app.use('/app', express.static(path.join(__dirname, 'public')));
+
 app.use('/web3', express.static(path.join(__dirname, 'node_modules', 'web3', 'dist', 'web3.min.js')));
 
+//supply contract abi's and addresses to client
+app.get('/contracts', (req, res, next) => {
+  res.status(200).json(data);
+})
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('App works'))
+app.listen(PORT, () => console.log('App works'));
