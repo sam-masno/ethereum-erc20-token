0874f123322c82d753421c58245c9629d45d7478
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-19 11:03:26 +0400

    added static paths for web3

--- client/server.js
+++ client/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/app', express.static(path.join(__dirname, 'public')));
+app.use('/web3', express.static(path.join(__dirname, 'node_modules', 'web3', 'dist', 'web3.min.js')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('App works'))
