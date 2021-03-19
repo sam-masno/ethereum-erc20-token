0f62304c2c25931dc455304eda16545068faf108
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-19 11:03:54 +0400

    updated deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const TokenSale = artifacts.require('../contract/TokenSale.sol');
 
 module.exports = async (deployer) => {
   const [admin] = await web3.eth.getAccounts();
-  const token = await deployer.deploy(Token, 'Test Token', 'TST',  1000000, 0);
+  const token = await deployer.deploy(Token, 'Test Token', 'TEST',  1000000, 0);
   const tokenSale = await deployer.deploy(TokenSale, token.address, 1000);
   token.transfer(tokenSale.address, 500000, { from: admin })
 }
