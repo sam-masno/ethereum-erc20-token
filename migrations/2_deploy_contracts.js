8b1758a2fd4f4b85ece65df4d0da0f984962e382
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-20 13:18:34 +0400

    added config variables for token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,14 +4,23 @@ const path = require('path');
 const Token = artifacts.require('../contracts/Token.sol');
 const TokenSale = artifacts.require('../contract/TokenSale.sol');
 
+const {
+  totalSupply,
+  name,
+  symbol,
+  decimal,
+  price,
+  initialTransferToTokenSale
+} = require('../tokenConfig.js')
+
 module.exports = async (deployer) => {
   // get admin wallet
   const [admin] = await web3.eth.getAccounts();
 
   //deploy contracts, 
-  const token = await deployer.deploy(Token, 'Test Token', 'TEST',  1000000, 0);
-  const tokenSale = await deployer.deploy(TokenSale, token.address, 1000);
-  token.transfer(tokenSale.address, 500000, { from: admin });
+  const token = await deployer.deploy(Token, name, symbol,  totalSupply, decimal);
+  const tokenSale = await deployer.deploy(TokenSale, token.address, price);
+  token.transfer(tokenSale.address, initialTransferToTokenSale, { from: admin });
 
   //write addresses to node app folder so they can be supplied to client
   const address = {
